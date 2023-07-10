2b2d3d72b6f9274e09d4d20e2fbbdf8941bf9cbc
Tomas Vopat <tomas.vopat@gmail.com>
2023-07-10 22:15:55 +0300

    Add program wrapper
    Also added support for input from a file.

--- README.md
+++ README.md
@@ -4,23 +4,9 @@ Command-line interface for the ChatGPT
 
 ## Installation
 
-1. Create virtual environemnt and install requirements
+1. Get your OpenAI key
+2. Execute the program
 
     ```
-    python3 -m venv venv
-    pip3 install -r requirements.txt
-    ```
-
-2. Create file with OpenAI key
-
-    ```
-    OPENAI_KEY="<your openai key>"
-    echo "OPENAI_KEY='${OPENAI_KEY}'" > .env
-    ```
-
-3. Active virtual enviroment and run the program
-
-    ```
-    source venv/bin/activate
-    python3 chatgpt.py
+    ./chatgpt
     ```
--- chatgpt.py
+++ chatgpt.py
@@ -1,51 +1,136 @@
 import openai
 import readline
-from decouple import config
+import argparse
+import decouple
 
-openai.api_key = config("OPENAI_KEY")
-model = 'gpt-3.5-turbo'
+openai.api_key = decouple.config("OPENAI_KEY")
+default_model = 'gpt-3.5-turbo'
 log_file_name = 'chatgpt.log'
 
-def logging(messages, filename):
-    log_file = open(log_file_name, "a")
-    log_file.write(str(messages))
+
+def temperature_check(value):
+    fvalue = float(value)
+    if fvalue < 0 or fvalue > 2:
+        raise argparse.ArgumentTypeError("Value can be from 0 to 2.")
+    return fvalue
+
+
+def logging(content, filename):
+    log_file = open(filename, "a")
+    log_file.write(str(content))
     log_file.close()
 
-def ask_chatgpt(messages):
-    answer = openai.ChatCompletion.create(model=model, messages=messages)
-    messages.append(answer.choices[0].message.to_dict())
-    return messages
 
+def print_help():
+    help_message = '''Instructions:
+    \help   print help
+    \clear  clear the context
+    \exit   exit the program
+    '''
+    print(help_message)
+
+
+parser = argparse.ArgumentParser(
+    prog='ChatGPT CLI',
+    description='Command-line interface for ChatGPT')
+parser.add_argument(
+    '--file',
+    help="File with input")
+parser.add_argument(
+    '--model',
+    choices=['gpt-3.5-turbo', 'gpt-4'],
+    default=default_model,
+    help="Which ChatGPT model to use")
+parser.add_argument(
+    '--temperature',
+    type=temperature_check,
+    help='Temperature of the GPT model. Float from 0 to 2',
+    default=0)
+args = parser.parse_args()
+
+print('Welcome to ChatGPT! Type "\help" for help.')
+
+file_input = ""
 messages = []
-total_tokens = 0
 
-print(
-'''Welcome to ChatGPT!
-\t- type 'clear' to remove context
-\t- type 'exit' to exit''')
+if args.file:
+    with open(args.file, 'r') as f:
+        file_input = f.read()
+if len(file_input):
+    messages.append({'role': 'user', 'content': file_input})
+    print("(new)> file input...")
 
-while(True):
-    if(not len(messages)):
-        user_input = input("(new)> ")
-    else:
-        user_input = input("(follow: {})> ".format(total_tokens))
+
+def read_input(is_new):
+    prompt = "(new)> "
+    if not is_new:
+        prompt = "(follow)> "
+
+    try:
+        user_input = input(prompt)
+    except EOFError:
+        print()
+        return (1, "EOF")
 
     user_input_trim = user_input.lower().strip()
-    if(user_input_trim == 'clear'):
-        messages = []
-        print("--- new context ---")
-        continue
-    elif(user_input_trim == ''):
-        print("empty prompt...")
-        continue
-    elif(user_input_trim == 'exit'):
-        print("Bye!")
-        break
+    if user_input_trim == '\exit':
+        return (1, "exit")
+    elif user_input_trim == '\clear':
+        return (2, "clear")
+    elif user_input_trim == '\help':
+        print_help()
+        return (3, "help")
+    elif user_input_trim == '':
+        return (4, "no_input")
+    else:
+        return (0, user_input)
 
-    messages.append({'role': 'user', 'content': user_input})
-    answer = openai.ChatCompletion.create(model=model, messages=messages)
-    total_tokens = answer.usage.total_tokens
-    messages.append(answer.choices[0].message.to_dict())
 
-    print(messages[-1]['content'])
+while (True):
+    empty_messages = (len(messages) == 0)
+    if empty_messages or (messages[-1]['role'] != 'user'):
+        (exit_code, new_message) = read_input(empty_messages)
+        if exit_code == 0:
+            messages.append({'role': 'user', 'content': new_message})
+        elif exit_code == 1:
+            break
+        elif exit_code == 2:
+            messages = []
+            continue
+        elif exit_code == 3:
+            continue
+        elif exit_code == 4:
+            continue
+        else:
+            print("unknown error")
+            exit()
+
+    try:
+        full_content = ""
+        answer = openai.ChatCompletion.create(
+            model=args.model,
+            temperature=args.temperature,
+            messages=messages,
+            stream=True)
+        for chunk in answer:
+            finish_reason = str(chunk.choices[0]["finish_reason"])
+            if finish_reason == "stop":
+                break
+            elif finish_reason == "None":
+                delta = chunk.choices[0].delta
+                if delta != {}:
+                    full_content = full_content + delta.content
+                    print(delta.content, end="")
+            else:
+                print("bad finish_reason:")
+                print(chunk)
+                exit()
+        messages.append({'role': 'assistant', 'content': full_content})
+        print()
+    except Exception as err:
+        print(err)
+        exit()
+
     logging(messages, log_file_name)
+
+print("Bye!")
